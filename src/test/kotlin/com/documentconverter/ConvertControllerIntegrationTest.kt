df6e80c79c35949752f97e0afc4ff8c15bb22d52
Pushkarev Nikolay <nikolazmailz@gmail.com>
2025-12-08 19:27:35 +0300

    move package

--- src/test/kotlin/com/documentconverter/ConvertControllerIntegrationTest.kt
+++ src/test/kotlin/com/documentconverter/ConvertControllerIntegrationTest.kt
@@ -1,4 +1,4 @@
-package com.example.documentconverter
+package com.documentconverter
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertFalse
