bee69068b9a50de732550087b8827be60a0f9a28
nikolazmailz <nikolazmailz@gmail.com>
2025-12-08 18:49:15 +0300

    Add application configuration

---
+++ src/main/kotlin/com/example/documentconverter/DocumentConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.documentconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DocumentConverterApplication
+
+fun main(args: Array<String>) {
+    runApplication<DocumentConverterApplication>(*args)
+}
