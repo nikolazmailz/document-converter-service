10b08c3992fe1d5a3dd214a1493fd000754cbbef
Pushkarev Nikolay <nikolazmailz@gmail.com>
2025-12-08 18:50:23 +0300

    moved package

--- src/main/kotlin/com/documentconverter/DocumentConverterApplication.kt
+++ src/main/kotlin/com/documentconverter/DocumentConverterApplication.kt
@@ -1,4 +1,4 @@
-package com.example.documentconverter
+package com.documentconverter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
